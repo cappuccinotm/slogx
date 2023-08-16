4cf7d7c1cebcca59b48ce0dd07d904e9cc548e41
Yelshat Duskaliyev <ura2178@gmail.com>
2023-08-17 02:55:19 +0600

    feat: added trimmer middleware for log attributes

---
+++ trim.go
@@ -0,0 +1,90 @@
+package slogx
+
+import (
+	"context"
+	"encoding"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"reflect"
+)
+
+// TrimAttrs returns a middleware that trims attributes of String and Any kind
+// to the provided limit.
+func TrimAttrs(limit int) Middleware {
+	return func(next HandleFunc) HandleFunc {
+		return func(ctx context.Context, rec slog.Record) error {
+			var nattrs []slog.Attr
+			hasOversizedAttrs := false
+			rec.Attrs(func(attr slog.Attr) bool {
+				nattr, trimmed := trim(limit, attr)
+				nattrs = append(nattrs, nattr)
+				hasOversizedAttrs = hasOversizedAttrs || trimmed
+				return true
+			})
+
+			if !hasOversizedAttrs {
+				return next(ctx, rec)
+			}
+
+			nrec := slog.NewRecord(rec.Time, rec.Level, rec.Message, rec.PC)
+			nrec.AddAttrs(nattrs...)
+
+			return next(ctx, nrec)
+		}
+	}
+}
+
+func trim(limit int, attr slog.Attr) (res slog.Attr, trimmed bool) {
+	attr.Value = attr.Value.Resolve()
+
+	str := ""
+	switch attr.Value.Kind() {
+	case slog.KindString:
+		str = attr.Value.String()
+	case slog.KindAny:
+		a := attr.Value.Any()
+
+		if tm, ok := a.(encoding.TextMarshaler); ok {
+			data, _ := tm.MarshalText()
+			str = string(data)
+			break
+		}
+
+		if jm, ok := a.(json.Marshaler); ok {
+			data, _ := jm.MarshalJSON()
+			str = string(data)
+			break
+		}
+
+		if bs, ok := byteSlice(a); ok {
+			str = string(bs)
+			break
+		}
+
+		str = fmt.Sprintf("%+v", a)
+	default:
+		return attr, false
+	}
+
+	if len(str) > limit {
+		str = str[:limit] + "..."
+	}
+
+	return slog.String(attr.Key, str), true
+}
+
+// byteSlice returns its argument as a []byte if the argument's
+// underlying type is []byte, along with a second return value of true.
+// Otherwise, it returns nil, false.
+func byteSlice(a any) ([]byte, bool) {
+	if bs, ok := a.([]byte); ok {
+		return bs, true
+	}
+	// Like Printf's %s, we allow both the slice type and the byte element type to be named.
+	t := reflect.TypeOf(a)
+	if t != nil && t.Kind() == reflect.Slice && t.Elem().Kind() == reflect.Uint8 {
+		return reflect.ValueOf(a).Bytes(), true
+	}
+	return nil, false
+}
---
+++ trim_test.go
@@ -0,0 +1,201 @@
+package slogx
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"log/slog"
+	"net/netip"
+	"runtime"
+	"testing"
+	"time"
+)
+
+func TestTrimAttrs(t *testing.T) {
+	t.Run("no attrs", func(t *testing.T) {
+		mw := TrimAttrs(10)
+		h := mw(func(ctx context.Context, rec slog.Record) error {
+			assert.Equal(t, 0, rec.NumAttrs())
+			return nil
+		})
+
+		err := h(context.Background(), slog.Record{})
+		require.NoError(t, err)
+	})
+
+	t.Run("not limitable attr", func(t *testing.T) {
+		mw := TrimAttrs(10)
+		h := mw(func(ctx context.Context, rec slog.Record) error {
+			assert.Equal(t, 1, rec.NumAttrs())
+			rec.Attrs(func(attr slog.Attr) bool {
+				assert.Equal(t, "key", attr.Key)
+				assert.Equal(t, 12345678912.3456789, attr.Value.Float64())
+				return true
+			})
+			return nil
+		})
+
+		var pcs [1]uintptr
+		runtime.Callers(2, pcs[:])
+		rec := slog.NewRecord(time.Now(), slog.LevelDebug, "test", pcs[0])
+		rec.Add("key", 12345678912.3456789)
+		err := h(context.Background(), rec)
+		require.NoError(t, err)
+	})
+
+	t.Run("not oversized attr", func(t *testing.T) {
+		mw := TrimAttrs(10)
+		h := mw(func(ctx context.Context, rec slog.Record) error {
+			assert.Equal(t, 1, rec.NumAttrs())
+			rec.Attrs(func(attr slog.Attr) bool {
+				assert.Equal(t, "key", attr.Key)
+				assert.Equal(t, "value", attr.Value.String())
+				return true
+			})
+			return nil
+		})
+
+		var pcs [1]uintptr
+		runtime.Callers(2, pcs[:])
+		rec := slog.NewRecord(time.Now(), slog.LevelDebug, "test", pcs[0])
+		rec.Add("key", "value")
+
+		err := h(context.Background(), rec)
+		require.NoError(t, err)
+	})
+
+	t.Run("oversized string attr", func(t *testing.T) {
+		mw := TrimAttrs(5)
+		h := mw(func(ctx context.Context, rec slog.Record) error {
+			assert.Equal(t, 1, rec.NumAttrs())
+			rec.Attrs(func(attr slog.Attr) bool {
+				assert.Equal(t, "key", attr.Key)
+				assert.Equal(t, "value...", attr.Value.String())
+				return true
+			})
+			return nil
+		})
+
+		var pcs [1]uintptr
+		runtime.Callers(2, pcs[:])
+		rec := slog.NewRecord(time.Now(), slog.LevelDebug, "test", pcs[0])
+		rec.Add("key", "value_very_long")
+
+		err := h(context.Background(), rec)
+		require.NoError(t, err)
+	})
+
+	t.Run("oversized text marshaler attr", func(t *testing.T) {
+		mw := TrimAttrs(11)
+		h := mw(func(ctx context.Context, rec slog.Record) error {
+			assert.Equal(t, 1, rec.NumAttrs())
+			rec.Attrs(func(attr slog.Attr) bool {
+				assert.Equal(t, "key", attr.Key)
+				assert.Equal(t, "127.127.127...", attr.Value.String())
+				return true
+			})
+			return nil
+		})
+
+		var pcs [1]uintptr
+		runtime.Callers(2, pcs[:])
+		rec := slog.NewRecord(time.Now(), slog.LevelDebug, "test", pcs[0])
+		addr, err := netip.ParseAddr("127.127.127.127")
+		require.NoError(t, err)
+		rec.Add("key", addr)
+
+		err = h(context.Background(), rec)
+		require.NoError(t, err)
+	})
+
+	t.Run("oversized json marshaler attr", func(t *testing.T) {
+		mw := TrimAttrs(11)
+		h := mw(func(ctx context.Context, rec slog.Record) error {
+			assert.Equal(t, 1, rec.NumAttrs())
+			rec.Attrs(func(attr slog.Attr) bool {
+				assert.Equal(t, "key", attr.Key)
+				assert.Equal(t, "some very l...", attr.Value.String())
+				return true
+			})
+			return nil
+		})
+
+		var pcs [1]uintptr
+		runtime.Callers(2, pcs[:])
+		rec := slog.NewRecord(time.Now(), slog.LevelDebug, "test", pcs[0])
+		rec.Add("key", json.RawMessage("some very long string"))
+
+		err := h(context.Background(), rec)
+		require.NoError(t, err)
+	})
+
+	t.Run("oversized byte slice attr", func(t *testing.T) {
+		mw := TrimAttrs(11)
+		h := mw(func(ctx context.Context, rec slog.Record) error {
+			assert.Equal(t, 1, rec.NumAttrs())
+			rec.Attrs(func(attr slog.Attr) bool {
+				assert.Equal(t, "key", attr.Key)
+				assert.Equal(t, "some very l...", attr.Value.String())
+				return true
+			})
+			return nil
+		})
+
+		var pcs [1]uintptr
+		runtime.Callers(2, pcs[:])
+		rec := slog.NewRecord(time.Now(), slog.LevelDebug, "test", pcs[0])
+		rec.Add("key", []byte("some very long string"))
+
+		err := h(context.Background(), rec)
+		require.NoError(t, err)
+	})
+
+	t.Run("oversized byte slice (custom named) attr", func(t *testing.T) {
+		mw := TrimAttrs(11)
+		h := mw(func(ctx context.Context, rec slog.Record) error {
+			assert.Equal(t, 1, rec.NumAttrs())
+			rec.Attrs(func(attr slog.Attr) bool {
+				assert.Equal(t, "key", attr.Key)
+				assert.Equal(t, "some very l...", attr.Value.String())
+				return true
+			})
+			return nil
+		})
+
+		var pcs [1]uintptr
+		runtime.Callers(2, pcs[:])
+		rec := slog.NewRecord(time.Now(), slog.LevelDebug, "test", pcs[0])
+		type trickyByteSlice []uint8
+		rec.Add("key", trickyByteSlice("some very long string"))
+
+		err := h(context.Background(), rec)
+		require.NoError(t, err)
+	})
+
+	t.Run("oversized unserializable attr", func(t *testing.T) {
+		mw := TrimAttrs(5)
+		h := mw(func(ctx context.Context, rec slog.Record) error {
+			assert.Equal(t, 1, rec.NumAttrs())
+			rec.Attrs(func(attr slog.Attr) bool {
+				assert.Equal(t, "key", attr.Key)
+				assert.Equal(t, "{a:12...", attr.Value.String())
+				return true
+			})
+			return nil
+		})
+
+		type testStruct struct {
+			a int
+			b string
+		}
+
+		var pcs [1]uintptr
+		runtime.Callers(2, pcs[:])
+		rec := slog.NewRecord(time.Now(), slog.LevelDebug, "test", pcs[0])
+		rec.Add("key", testStruct{a: 1234567890, b: "abacaba"})
+
+		err := h(context.Background(), rec)
+		require.NoError(t, err)
+	})
+}
