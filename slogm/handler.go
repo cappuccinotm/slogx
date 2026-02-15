c0ce466cd6c6c97ee571c7e7f1e59809625e228f
Yelshat Duskaliyev <yduskaliyev@semior.dev>
2026-02-16 02:43:47 +0500

    fix: ignore intermediate handler's error

--- slogm/handler.go
+++ slogm/handler.go
@@ -8,11 +8,12 @@ import (
 )
 
 // ApplyHandler wraps slog.Handler as Middleware.
+// Error from the handler is ignored, but the handler is called only if it is enabled for the record's level.
 func ApplyHandler(handler slog.Handler) slogx.Middleware {
 	return func(next slogx.HandleFunc) slogx.HandleFunc {
 		return func(ctx context.Context, rec slog.Record) error {
 			if handler.Enabled(ctx, rec.Level) {
-				return handler.Handle(ctx, rec)
+				_ = handler.Handle(ctx, rec)
 			}
 
 			return next(ctx, rec)
